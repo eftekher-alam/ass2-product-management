da50360cac9a38c54f93545919e758c2e82116c2
eftekher-alam <eftekher@yahoo.com>
2024-10-02 20:01:03 +0600

    file structure and route setup done for product

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { ProductRoutes } from "./app/modules/product/product.route";
 
 const app = express();
 
@@ -7,11 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/api", ProductRoutes);
+
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!");
-});
-app.get("/bye", (req: Request, res: Response) => {
-    res.send("Good Night...!");
 });
 
 export default app;
