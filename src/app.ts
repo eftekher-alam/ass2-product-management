e7f007581a709b24f905ad9e677ea6bdb599f011
eftekher-alam <eftekher@yahoo.com>
2024-10-04 19:08:46 +0600

    No route found error added

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,8 @@ app.use(cors());
 app.use("/api", ProductRoutes);
 app.use("/api", OrderRouters);
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World!");
+app.use((req, res, next) => {
+    res.status(404).json({ message: "Route not found" });
 });
 
 export default app;
